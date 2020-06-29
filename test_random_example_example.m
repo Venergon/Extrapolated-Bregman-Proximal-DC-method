60927c990d1ddb3137eb7aea77ae7beba6bb23a0
Rowella <alex@gormand.com.au>
2020-06-29 21:39:58 +1000

    Added least squares solution for comparison
    Before I was only comparing with x_hat, which made it hard to tell whether the optimisation was successful (as x_hat was already both very close to the optimum for ||Ax - b|| and quite sparse)

--- test_random_example_example.m
+++ test_random_example_example.m
@@ -1,13 +1,13 @@
 % Test ExtendedProximalDCMethod using a randomly generated matrix of size
 % nXm, with some gaussian noise
-USE_2_NORM = false;
+USE_2_NORM = true;
 rtol = 1e-3;
 lambda = 10;
 n = 100;
 m = 101;
 density = 0.5;
 noise_mu = 0;
-noise_sigma = 0.5;
+noise_sigma = 1;
 
 A = rand(n, m);
 
@@ -37,11 +37,16 @@ stop_fn = @(x_prev, x_curr)(obj_fn(x_curr) >= obj_fn(x_prev) && obj_fn(x_curr) -
 x_denoised = ExtendedProximalDCMethod(A, b, x0, dg, lambda, stop_fn);
 b_denoised = A*x_denoised;
 
+x_least_squares = A \ b;
+b_least_squares = A*x_least_squares;
+
 obj_hat = obj_fn(x_hat);
 obj_denoised = obj_fn(x_denoised);
+obj_least_squares = obj_fn(x_least_squares);
 
-b_diff_denoised = norm(b_denoised - b, 2);
-b_diff_hat = norm(b_hat - b, 2);
+b_diff_denoised = norm(b_denoised - b, 2)/norm(b, 2);
+b_diff_hat = norm(b_hat - b, 2)/norm(b, 2);
+b_diff_least_squares = norm(b_least_squares - b, 2)/norm(b, 2);
 
 function [dg] = dg_2_norm(x) 
     if x == 0
