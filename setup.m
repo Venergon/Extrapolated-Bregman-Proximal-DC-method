c4af0daf8ea4062decd827eb0cd5827ac876fe91
Rowella <alex@gormand.com.au>
2020-07-27 16:47:23 +1000

    Basic image testing with noisy image

--- setup.m
+++ setup.m
@@ -1,2 +1,7 @@
-addpath('penalty_functions');
+% Setup the paths needed to run all of the code
 addpath('argmin_functions');
+addpath('argmin_functions/cubic_solvers');
+
+addpath('image_manipulation');
+
+addpath('penalty_functions');
